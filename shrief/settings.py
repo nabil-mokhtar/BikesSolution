444f3f1144578191e535c8da56419808eac77196
Ahmed Mostafa <tefa2436@gmail.com>
2020-07-26 17:41:34 +0200

    Protect Sensitive Data

--- shrief/settings.py
+++ shrief/settings.py
@@ -1,16 +1,17 @@
 
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y!8(e19fdh7li@kvrrb^3r_n3!e!b-7m=(pv+ru3cznb-=^p@v'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
