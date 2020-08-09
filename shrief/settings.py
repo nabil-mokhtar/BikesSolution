f8f6125bec94b9bdec485e51926ace9cf1f2d6da
Ahmed Mostafa <63076161+A7med650@users.noreply.github.com>
2020-08-09 05:47:03 +0200

    Update settings.py

--- shrief/settings.py
+++ shrief/settings.py
@@ -131,6 +131,6 @@ MEDIA_URL = '/media/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'am8540689@gmail.com'
-EMAIL_HOST_PASSWORD = 'dezixceejltwaywu'
+EMAIL_HOST_PASSWORD = 'key'
 EMAIL_USE_TLS = True
 EMAIL_PORT = '587'
